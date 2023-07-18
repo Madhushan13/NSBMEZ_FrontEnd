707d290555313fa5f3d13dbf03e07a0a8cac7fdc
Akila Tharinda <96075103+Akilap11@users.noreply.github.com>
2023-07-18 23:15:27 +0530

    login_page

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -5,11 +5,98 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Center(
-        child: Text('This is the login page.'),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: Center(
+              child: FractionallySizedBox(
+                widthFactor: 0.5,
+                heightFactor: 1,
+                child: Image.asset(
+                  'assets/images/usermaleicon-1-497.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  // Username
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  // Password
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  // Sign In Button
+                  ElevatedButton(
+                    onPressed: () {
+                      // LINK SIGN IN HERE
+                    },
+                    child: const Text('Sign In'),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  // Forgot Password
+                  GestureDetector(
+                    onTap: () {
+                      // LINK FORGOT PW HERE
+                    },
+                    child: const Text('Forgot Password?'),
+                  ),
+
+                  const Spacer(),
+
+                  //sign UP
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Don\'t have an account? ',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // LINK SIGN UP HERE
+                        },
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
