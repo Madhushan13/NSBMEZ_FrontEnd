f1f5e28c499ff680aa8f5fa4ae37182dd32efe60
himeshapathirana <pathiranathimesha2021@gmail.com>
2023-08-02 22:05:50 +0530

    added signup_page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application/Pages/home_page.dart';
 import 'package:flutter_application/Pages/loading_page.dart';
 import 'package:flutter_application/Pages/results_page.dart';
+import 'package:madproject/singup_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: ResultsPage());
+    //return const MaterialApp(home: ResultsPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+      home: const SignupPage(),
+      
+     
+ 
+    );
   }
 }
