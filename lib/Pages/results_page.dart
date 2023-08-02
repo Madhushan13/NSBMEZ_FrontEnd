d503065a12ed2bba072ea581c626bfd614db7d15
Akila Tharinda <96075103+Akilap11@users.noreply.github.com>
2023-08-02 20:47:41 +0530

    update

--- lib/Pages/results_page.dart
+++ lib/Pages/results_page.dart
@@ -12,177 +12,185 @@ class ResultsPage extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 110,
-          flexibleSpace: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Results'),
+            actions: [
+              // Notification Icon
+              IconButton(
+                onPressed: () {
+                  // Add your notification logic here
+                },
+                icon: const Icon(Icons.notifications),
+              ),
+              // EndDrawer Icon
+              Builder(
+                builder: (context) => IconButton(
+                  onPressed: () {
+                    Scaffold.of(context).openEndDrawer();
+                  },
+                  icon: const Icon(Icons.menu),
+                ),
+              ),
+            ],
+          ),
+          endDrawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
               children: [
-                // First Row (Logo and Text "SEARCH")
-                const Padding(
-                  padding: EdgeInsets.all(14.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                const UserAccountsDrawerHeader(
+                  accountName: Text('John Doe'),
+                  accountEmail: Text('johndoe@example.com'),
+                  currentAccountPicture: CircleAvatar(
+                    child: Icon(Icons.person),
                   ),
                 ),
-                // Second Row (Search Bar and Menu Icon)
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Search Bar
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 40, 45, 51),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          height: 50,
-                          padding: const EdgeInsets.symmetric(horizontal: 12),
-                          child: Row(
-                            children: [
-                              //ICONNNN
-                              IconButton(
-                                icon: const Icon(Icons.search),
-                                color: const Color.fromARGB(255, 25, 240, 6),
-                                onPressed: () {
-                                  //LINK HEREEEEEEEEEEEEEEEE
-                                },
-                              ),
-                              const Expanded(
-                                child: TextField(
-                                  style: TextStyle(
-                                    color: Colors
-                                        .black, // Customize the font color of the user-entered text
-                                  ),
-                                  decoration: InputDecoration(
-                                    hintText:
-                                        'S e a r c h   H e r e  . . . . . .',
-                                    hintStyle: TextStyle(
-                                      color:
-                                          kHintTextColor, // Use the constant color for hint text
-                                    ),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      // Menu Icon
-                      Padding(
-                        padding: const EdgeInsets.only(right: 6, left: 0),
-                        child: IconButton(
-                          iconSize: 40,
-                          icon: const Icon(Icons.menu),
-                          color: const Color.fromARGB(255, 12, 12, 12),
-                          onPressed: () {
-                            // Handle the menu button press here
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
+                ListTile(
+                  leading: const Icon(Icons.account_circle),
+                  title: const Text('Profile'),
+                  onTap: () {
+                    // Add your profile navigation logic here
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.directions_bus),
+                  title: const Text('Transport Service'),
+                  onTap: () {
+                    // Add your transport service navigation logic here
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.table_chart),
+                  title: const Text('Time Table'),
+                  onTap: () {
+                    // Add your time table navigation logic here
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.qr_code),
+                  title: const Text('Take Attendance/QR'),
+                  onTap: () {
+                    // Add your attendance/QR navigation logic here
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.event),
+                  title: const Text('Events'),
+                  onTap: () {
+                    // Add your events navigation logic here
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.poll),
+                  title: const Text('Result'),
+                  onTap: () {
+                    // Add your result navigation logic here
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.logout),
+                  title: const Text('Logout'),
+                  onTap: () {
+                    // Add your logout navigation logic here
+                  },
                 ),
               ],
             ),
           ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10, top: 40),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        onPressed: () {}, child: const Text("Year 1")),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        onPressed: () {}, child: const Text("Year 2")),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        onPressed: () {}, child: const Text("Year 3")),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        onPressed: () {}, child: const Text("Year 4")),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        onPressed: () {}, child: const Text("Semester 1")),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        onPressed: () {}, child: const Text("Semester 2")),
-                  ),
-                ],
-              ),
-              //adding space
-              const SizedBox(
-                height: 30,
-              ),
-              DataTable(
-                columns: const [
-                  DataColumn(
-                      label: Text('Module Code',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold))),
-                  DataColumn(
-                      label: Text('module',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold))),
-                  DataColumn(
-                      label: Text('Result',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold))),
-                ],
-                rows: const [
-                  DataRow(cells: [
-                    DataCell(Center(child: Text('1'))),
-                    DataCell(Center(child: Text('ss'))),
-                    DataCell(Center(child: Text('ss'))),
-                  ]),
-                  DataRow(cells: [
-                    DataCell(Center(child: Text('1'))),
-                    DataCell(Center(child: Text('ss'))),
-                    DataCell(Center(child: Text('ss'))),
-                  ]),
-                  DataRow(cells: [
-                    DataCell(Center(child: Text('1'))),
-                    DataCell(Center(child: Text('ss'))),
-                    DataCell(Center(child: Text('ss'))),
-                  ]),
-                  DataRow(cells: [
-                    DataCell(Center(child: Text('1'))),
-                    DataCell(Center(child: Text('ss'))),
-                    DataCell(Center(child: Text('ss'))),
-                  ]),
-                  DataRow(cells: [
-                    DataCell(Center(child: Text('1'))),
-                    DataCell(Center(child: Text('ss'))),
-                    DataCell(Center(child: Text('ss'))),
-                  ]),
-                ],
-              ),
-            ],
+          body: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 40),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text("Year 1")),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text("Year 2")),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text("Year 3")),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text("Year 4")),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text("Semester 1")),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text("Semester 2")),
+                    ),
+                  ],
+                ),
+                //adding space
+                const SizedBox(
+                  height: 30,
+                ),
+                DataTable(
+                  columns: const [
+                    DataColumn(
+                        label: Text('Module Code',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold))),
+                    DataColumn(
+                        label: Text('module',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold))),
+                    DataColumn(
+                        label: Text('Result',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold))),
+                  ],
+                  rows: const [
+                    DataRow(cells: [
+                      DataCell(Center(child: Text('1'))),
+                      DataCell(Center(child: Text('ss'))),
+                      DataCell(Center(child: Text('ss'))),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Center(child: Text('1'))),
+                      DataCell(Center(child: Text('ss'))),
+                      DataCell(Center(child: Text('ss'))),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Center(child: Text('1'))),
+                      DataCell(Center(child: Text('ss'))),
+                      DataCell(Center(child: Text('ss'))),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Center(child: Text('1'))),
+                      DataCell(Center(child: Text('ss'))),
+                      DataCell(Center(child: Text('ss'))),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Center(child: Text('1'))),
+                      DataCell(Center(child: Text('ss'))),
+                      DataCell(Center(child: Text('ss'))),
+                    ]),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/Pages/home_page.dart';
 import 'package:flutter_application/Pages/loading_page.dart';
+import 'package:flutter_application/Pages/results_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoadingPage());
+    return const MaterialApp(home: ResultsPage());
   }
 }
