204bafd4b6290c8b99a9a5e88f252b054a5bbf47
Madhushan13 <96137093+Madhushan13@users.noreply.github.com>
2023-08-04 22:54:00 +0530

    update

--- lib/Pages/all_widget_class.dart
+++ lib/Pages/all_widget_class.dart
@@ -1,8 +1,8 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
-//handling the carousel and the indicator
 class CarouselWithIndicator extends StatefulWidget {
   const CarouselWithIndicator({Key? key}) : super(key: key);
 
@@ -80,54 +80,116 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
       );
 }
 
-//Home card widget
-class HomeCard extends StatelessWidget {
-  final IconData icon;
+class RoundedHomeCard extends StatelessWidget {
   final String label;
   final VoidCallback onTap;
+  final String gifImagePath;
 
-  const HomeCard({
+  const RoundedHomeCard({
     Key? key,
-    required this.icon,
     required this.label,
     required this.onTap,
+    required this.gifImagePath,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width:
-          MediaQuery.of(context).size.width * 0.3, // Fixed width for the card
-      height: 120, // Fixed height for the card
+    final cardSize = MediaQuery.of(context).size.width * 0.2;
+
+    return Container(
+      width: cardSize,
       child: Card(
+        color: Color.fromARGB(255, 8, 146, 5),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(40),
         ),
-        elevation: 30,
+        elevation: 5,
         child: InkWell(
           onTap: onTap,
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(30),
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: Column(
-              mainAxisAlignment:
-                  MainAxisAlignment.end, // Align text to the bottom
+              mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Icon(icon, size: 30, color: Color.fromARGB(255, 74, 187, 9)),
+                Container(
+                  width: cardSize * 0.5, // Adjust the width of the image
+                  height: cardSize * 0.3, // Adjust the height of the image
+                  child: Image.asset(
+                    gifImagePath,
+                    fit: BoxFit.cover, // Ensure the image covers the container
+                  ),
+                ),
                 const SizedBox(height: 8),
                 Text(
                   label,
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 9,
-                    color: Color.fromARGB(255, 6, 129, 245),
+                    fontSize: 10,
+                    color: Color.fromARGB(255, 67, 127, 59),
                   ),
                 ),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class LectureDetailsContainer extends StatelessWidget {
+  final String startTime;
+  final String endTime;
+  final String lectureName;
+  final String lectureHall;
+
+  const LectureDetailsContainer({
+    Key? key,
+    required this.startTime,
+    required this.endTime,
+    required this.lectureName,
+    required this.lectureHall,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Lecture Details',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Start Time: $startTime',
+            style: TextStyle(fontSize: 16),
+          ),
+          Text(
+            'End Time: $endTime',
+            style: TextStyle(fontSize: 16),
+          ),
+          Text(
+            'Lecture Name: $lectureName',
+            style: TextStyle(fontSize: 16),
+          ),
+          Text(
+            'Lecture Hall: $lectureHall',
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
       ),
     );
   }
--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/Pages/all_widget_class.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'all_widget_class.dart';
 import 'events_page.dart';
 import 'qr_scanner.dart';
 import 'time_table.dart';
@@ -11,6 +13,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 1, 1, 37),
         body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -22,123 +25,247 @@ class HomePage extends StatelessWidget {
                 padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Column(
                   children: [
+                    // First Row
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        HomeCard(
-                          icon: Icons.calendar_today,
-                          label: 'Academic Calendar',
-                          onTap: () {
-                            // Add your navigation logic here
-                          },
-                        ),
-                        HomeCard(
-                          icon: Icons.qr_code_scanner,
-                          label: 'QR Scanner',
-                          onTap: () {
-                            // Add your navigation logic here
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const QRScanner(),
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'Academic Calendar',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 6, 1210, 245),
                               ),
-                            );
-                          },
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        HomeCard(
-                          icon: Icons.access_time,
-                          label: 'Attendance',
-                          onTap: () {
-                            // Add your navigation logic here
-                          },
-                        ),
-                        HomeCard(
-                          icon: Icons.table_chart,
-                          label: 'Lecture Time Table',
-                          onTap: () {
-                            // Add your navigation logic here
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const TimeTable(),
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const QRScanner(),
+                                  ),
+                                );
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'QR Scanner',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 1, 14, 18),
                               ),
-                            );
-                            // Add your navigation logic here
-                          },
+                            ),
+                          ],
                         ),
-                        HomeCard(
-                          icon: Icons.event,
-                          label: 'Events',
-                          onTap: () {
-                            // Add your navigation logic here
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => EventsPage(),
-                                ));
-                          },
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'Attendance',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 9, 10, 10),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const TimeTable(),
+                                  ),
+                                );
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'Lecture Time Table',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 6, 9, 11),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    // Second Row
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => EventsPage(),
+                                  ),
+                                );
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'Events',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 10, 11, 11),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'GPA Calculator',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 9, 12, 14),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'Result',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 6, 7, 7),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'My Account',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 4, 10, 13),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
                     const SizedBox(height: 20),
+
+                    // Third Row
+
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        HomeCard(
-                          icon: Icons.calculate,
-                          label: 'GPA Calculator',
-                          onTap: () {
-                            // Add your navigation logic here
-                          },
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'Transport',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 6, 1210, 245),
+                              ),
+                            ),
+                          ],
                         ),
-                        HomeCard(
-                          icon: Icons.poll,
-                          label: 'Result',
-                          onTap: () {
-                            // Add your navigation logic here
-                          },
-                        ),
-                        HomeCard(
-                          icon: Icons.account_balance_wallet,
-                          label: 'My Account',
-                          onTap: () {
-                            // Add your navigation logic here
-                          },
+                        Column(
+                          children: [
+                            RoundedHomeCard(
+                              gifImagePath: 'assets/images/calender.gif',
+                              onTap: () {
+                                // Add your navigation logic here
+                              },
+                              label: '',
+                            ),
+                            const Text(
+                              'Payments',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Color.fromARGB(255, 6, 1210, 245),
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
+
                     const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        HomeCard(
-                          icon: Icons.directions_bus,
-                          label: 'Transport',
-                          onTap: () {
-                            // Add your navigation logic here
-                          },
-                        ),
-                        HomeCard(
-                          icon: Icons.chat,
-                          label: 'Chat',
-                          onTap: () {
-                            // Add your navigation logic here
-                          },
-                        ),
-                        HomeCard(
-                          icon: Icons.chat,
-                          label: 'Payments',
-                          onTap: () {
-                            // Add your navigation logic here
-                          },
-                        ),
-                      ],
+
+                    // Lecture Details Container
+                    const LectureDetailsContainer(
+                      startTime: '8:00 AM',
+                      endTime: '10:00 AM',
+                      lectureName: 'Mathematics',
+                      lectureHall: 'Room 201',
                     ),
                   ],
                 ),
