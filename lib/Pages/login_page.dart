d3d2a857eafc1337d19f7d9d1a58455907294227
Akila Tharinda <96075103+Akilap11@users.noreply.github.com>
2023-08-16 00:19:17 +0530

    Login&ResetPW

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -25,95 +25,148 @@ class LoginPage extends StatelessWidget {
             ),
           ),
           Expanded(
-            flex: 6,
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  // Username
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Username',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-
-                  // Password
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-
-                  // Sign In Button
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomePage()));
-                    },
-                    child: const Text('Sign In'),
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  // Forgot Password
-                  GestureDetector(
-                    onTap: () {
-                      //FORGOT PW LINK
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ResetPWPage()));
-                    },
-                    child: const Text(
-                      'Forgot Password?',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-
-                  const Spacer(),
-
-                  //sign UP
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            flex: 8,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              child: Container(
+                color: const Color(0xFF264F44),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      const Text(
-                        'Don\'t have an account? ',
-                        style: TextStyle(
-                          fontSize: 18,
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 20.0),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          //SIGN UP LINK
+                      // Username
+                      const TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Username',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+
+                      // Password
+                      const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Password',
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+
+                      // Sign In Button
+                      ElevatedButton(
+                        onPressed: () {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => const SignupPage()));
+                                  builder: (context) => const HomePage()));
+                        },
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(500, 50),
+                            backgroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      // Forgot Password
+                      GestureDetector(
+                        onTap: () {
+                          //FORGOT PW LINK
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const ResetPWPage()));
                         },
                         child: const Text(
-                          'Sign Up',
+                          'Forgot Password?',
                           style: TextStyle(
                             fontSize: 18,
                             color: Colors.blue,
                           ),
                         ),
                       ),
+
+                      const Spacer(),
+
+                      //sign UP
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account? ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              //SIGN UP LINK
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SignupPage()));
+                            },
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
--- lib/Pages/resetpw_page.dart
+++ lib/Pages/resetpw_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/Pages/home_page.dart';
+import 'package:flutter_application/Pages/signup_page.dart';
 
 class ResetPWPage extends StatelessWidget {
   const ResetPWPage({super.key});
@@ -6,8 +8,173 @@ class ResetPWPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("RESET PASSWORD"),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: Center(
+              child: FractionallySizedBox(
+                widthFactor: 0.5,
+                heightFactor: 1,
+                child: Image.asset(
+                  'assets/images/usermaleicon-1-497.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 8,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              child: Container(
+                color: const Color(0xFF264F44),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 20.0),
+                        child: Text(
+                          'Reset Password',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      // Username
+                      const TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Username',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+
+                      // Password
+                      const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Password',
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+
+                      //confirm pw
+                      const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Confirm Password',
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+
+                      // Sign In Button
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomePage()));
+                        },
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(500, 50),
+                            backgroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      const Spacer(),
+
+                      //sign UP
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account? ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              //SIGN UP LINK
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SignupPage()));
+                            },
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
