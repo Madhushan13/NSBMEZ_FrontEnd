4b37506a3240b2bcadaeac69d0b5daa5f8541a23
himeshapathirana <pathiranathimesha2021@gmail.com>
2023-09-07 12:51:13 +0530

    add signup

--- lib/Pages/signup_page.dart
+++ lib/Pages/signup_page.dart
@@ -15,25 +15,240 @@ class _SignupPageState extends State<SignupPage> {
   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        begin: Alignment.topRight,
-        end: Alignment.bottomLeft,
-        colors: [
-          Colors.white,
-          Colors.white,
+    return Scaffold(
+      body: Column(
+        children: [
+          const Expanded(
+            flex: 5,
+            child: Center(
+              child: FractionallySizedBox(
+                widthFactor: 0.5,
+                heightFactor: 1,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 8,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              child: Container(
+                color: const Color(0xFF264F44),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 20.0),
+                        child: Text(
+                          'SignUp',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      // Username
+                      TextField(
+                        controller: idcontroller,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Username',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        controller: idcontroller,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Email',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      // Password
+                      TextField(
+                        controller: pwcontroller,
+                        obscureText: !isPasswordVisible,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Password',
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      TextField(
+                        controller: idcontroller,
+                        obscureText: !isPasswordVisible,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Color(0xFFBAD3C8),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              )),
+                          fillColor: Color(0xFFBAD3C8),
+                          filled: true,
+                          hintText: 'Confirm password',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Column(
+                        children: [
+                          RadioListTile(
+                            title: const Text("Lecturer"),
+                            value: "Lecturer",
+                            groupValue: selectValue,
+                            onChanged: (value) {
+                              setState(() {
+                                selectValue = value.toString();
+                              });
+                            },
+                          ),
+                          RadioListTile(
+                            title: const Text("Admin"),
+                            value: "Admin",
+                            groupValue: selectValue,
+                            onChanged: (value) {
+                              setState(() {
+                                selectValue = value.toString();
+                              });
+                            },
+                          ),
+                          RadioListTile(
+                            title: const Text("Student"),
+                            value: "Student",
+                            groupValue: selectValue,
+                            onChanged: (value) {
+                              setState(() {
+                                selectValue = value.toString();
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          // Handle the sign-up logic here
+                        },
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(500, 50),
+                            backgroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      GestureDetector(
+                        onTap: () {
+                          // Handle forgot password logic here
+                        },
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account? ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              // Handle sign-up link logic here
+                            },
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
-      )),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: _page(),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: SignupPage(),
+  ));
+}
 
   // ignore: non_constant_identifier_names
-  Widget _page() {
+  /*Widget _page() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Center(
@@ -100,9 +315,9 @@ class _SignupPageState extends State<SignupPage> {
       child: const Icon(Icons.person,
           color: Color.fromARGB(255, 78, 78, 78), size: 120),
     );
-  }
+  }*/
 
-  Widget _inputField(String hintText, TextEditingController controller,
+  /*Widget _inputField(String hintText, TextEditingController controller,
       {isPassword = false}) {
     var border = OutlineInputBorder(
         borderRadius: BorderRadius.circular(18),
@@ -129,7 +344,7 @@ class _SignupPageState extends State<SignupPage> {
                 password: passwordController.text)
             .then((value) => {
                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginPage()))
+                      MaterialPageRoute(builder: (context) => const LoginPage()))
                 });
 
         debugPrint("Username : ${usernameController.text}");
@@ -166,4 +381,4 @@ class _SignupPageState extends State<SignupPage> {
       ],
     );
   }
-}
+}*/
