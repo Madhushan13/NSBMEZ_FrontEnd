7592a95993cb2a4c53be86b5be136e3a9df4041c
Pavan Dulakshitha <96134390+PavanEdirisingha2000@users.noreply.github.com>
2023-09-12 22:15:47 +0530

    gpaCalUpdate

--- lib/Pages/gpa_calculater.dart
+++ lib/Pages/gpa_calculater.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class GPACalculatorPage extends StatefulWidget {
   const GPACalculatorPage({super.key});
 
@@ -31,80 +29,83 @@ class _GPACalculatorPageState extends State<GPACalculatorPage> {
       appBar: AppBar(
         title: const Text('GPA Calculator'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              'Enter Grades and Credits for 10 Subjects',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            for (int i = 0; i < 10; i++)
-              Row(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: DropdownButton<double?>(
-                      value: grades[i],
-                      onChanged: (value) {
-                        setState(() {
-                          grades[i] = value;
-                        });
-                      },
-                      items: const [
-                        DropdownMenuItem(value: 4.0, child: Text('A')),
-                        DropdownMenuItem(value: 3.7, child: Text('A-')),
-                        DropdownMenuItem(value: 3.3, child: Text('B+')),
-                        DropdownMenuItem(value: 3.0, child: Text('B')),
-                        DropdownMenuItem(value: 2.7, child: Text('B-')),
-                        DropdownMenuItem(value: 2.3, child: Text('C+')),
-                        DropdownMenuItem(value: 2.0, child: Text('C')),
-                        
-
-                        
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Enter Grades and Credits for 10 Subjects',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              for (int i = 0; i < 10; i++)
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: DropdownButton<double?>(
+                        value: grades[i],
+                        onChanged: (value) {
+                          setState(() {
+                            grades[i] = value;
+                          });
+                        },
+                        items: const [
+                          DropdownMenuItem(value: 4.0, child: Text('A')),
+                          DropdownMenuItem(value: 3.7, child: Text('A-')),
+                          DropdownMenuItem(value: 3.3, child: Text('B+')),
+                          DropdownMenuItem(value: 3.0, child: Text('B')),
+                          DropdownMenuItem(value: 2.7, child: Text('B-')),
+                          DropdownMenuItem(value: 2.3, child: Text('C+')),
+                          DropdownMenuItem(value: 2.0, child: Text('C')),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(labelText: 'Credits'),
+                        onChanged: (value) {
+                          setState(() {
+                            credits[i] = double.tryParse(value) ?? 0;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ElevatedButton(
+                onPressed: () {
+                  double gpa = calculateGPA();
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('GPA Calculation'),
+                      content: Text('Your GPA is: ${gpa.toStringAsFixed(2)}'),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('OK'),
+                        ),
                       ],
                     ),
-                  ),
-                  const SizedBox(width: 20),
-                  Expanded(
-                    flex: 2,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(labelText: 'Credits'),
-                      onChanged: (value) {
-                        setState(() {
-                          credits[i] = double.tryParse(value) ?? 0;
-                        });
-                      },
-                    ),
-                  ),
-                ],
+                  );
+                },
+                child: const Text('Calculate GPA'),
               ),
-            ElevatedButton(
-              onPressed: () {
-                double gpa = calculateGPA();
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('GPA Calculation'),
-                    content: Text('Your GPA is: ${gpa.toStringAsFixed(2)}'),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('OK'),
-                      ),
-                    ],
-                  ),
-                );
-              },
-              child: const Text('Calculate GPA'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(home: GPACalculatorPage()));
 }
