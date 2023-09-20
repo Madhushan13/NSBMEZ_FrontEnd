12acad99c15aa2d0e0803fab6ae5f78da62e2035
dkjayalal <94827804+dkjayalal@users.noreply.github.com>
2023-09-20 19:42:42 +0530

    tt change

--- lib/Pages/time_table.dart
+++ lib/Pages/time_table.dart
@@ -9,47 +9,71 @@ class TimeTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SfCalendar(
-          view: CalendarView.week,
-          firstDayOfWeek: 1,
-          //initialDisplayDate: DateTime(2023, 06, 05, 09, 00, 00),
-          //initialSelectedDate: DateTime(2023, 06, 05, 09, 00, 00),
-          dataSource: MeetingDateSource(getAppoinments()),
-        ),
-        bottomNavigationBar: CustomBottomNavigationBar(
-          currentIndex: 2, // Set the correct index for EventsPage
-          onTap: (index) {
-            // Handle bottom navigation bar tap event
-            if (index == 0) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
-              );
-            }
-          },
+    return DefaultTabController(
+      length: 2, // Number of tabs (Day and Week views)
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Time Table'),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Day View'),
+                Tab(text: 'Week View'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              // Day View
+              SfCalendar(
+                view: CalendarView.day,
+                firstDayOfWeek: 1,
+                dataSource: MeetingDateSource(getAppointments()),
+              ),
+              // Week View
+              SfCalendar(
+                view: CalendarView.week,
+                firstDayOfWeek: 1,
+                dataSource: MeetingDateSource(getAppointments()),
+              ),
+            ],
+          ),
+          bottomNavigationBar: CustomBottomNavigationBar(
+            currentIndex: 2, // Set the correct index for EventsPage
+            onTap: (index) {
+              // Handle bottom navigation bar tap event
+              if (index == 0) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
   }
 }
 
-List<Appointment> getAppoinments() {
+List<Appointment> getAppointments() {
   List<Appointment> meetings = <Appointment>[];
   final DateTime today = DateTime.now();
   final DateTime startTime =
       DateTime(today.year, today.month, today.day, 9, 0, 0); // Start at 9 am
   final DateTime endTime =
-      DateTime(today.year, today.month, today.day, 12, 0, 0); // End at 5 pm
+      DateTime(today.year, today.month, today.day, 12, 0, 0); // End at 12 pm
 
   meetings.add(Appointment(
     startTime: startTime,
     endTime: endTime,
-    subject: 'MAD -Mobile Application Development',
+    subject: 'MAD',
     color: Colors.yellow,
-    recurrenceRule: 'FREQ=DAILY;INTERVAL=1;COUNT=10',
+    recurrenceRule: 'FREQ=WEEKLY;INTERVAL=1;COUNT=03',
   ));
+  
+  // Add more appointments for day and week views as needed
+
   return meetings;
 }
 
