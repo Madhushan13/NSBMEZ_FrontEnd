98361687672be5c065ec1143cc32763a25dd26b3
himeshapathirana <94542536+himeshapathirana@users.noreply.github.com>
2023-09-21 10:38:14 +0530

    Update payment_details.dart

--- lib/Pages/payment_details.dart
+++ lib/Pages/payment_details.dart
@@ -3,49 +3,17 @@ import 'package:flutter/material.dart';
 import 'Home_widget.dart';
 import 'home_page.dart';
 
-// ignore: camel_case_types
 class Online_Payment extends StatelessWidget {
   const Online_Payment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: IconButton(
-            icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          toolbarHeight: 80,
-          actions: [
-            Builder(
-              builder: (context) => IconButton(
-                color: Colors.black,
-                onPressed: () {
-                  Scaffold.of(context).openEndDrawer();
-                },
-                icon: const Icon(Icons.menu),
-              ),
-            ),
-          ],
-          flexibleSpace: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/NSBMEZ Black.png',
-                height: 100,
-                width: 140,
-                fit: BoxFit.contain,
-              ),
-            ],
-          ),
-        ),
-        body: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Online Payment'),
+      ),
+      body: SingleChildScrollView( 
+        child: Column(
           children: [
             Container(
               color: const Color(0xFF1B6E4F),
@@ -62,42 +30,40 @@ class Online_Payment extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16.0),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Column(
-                  children: [
-                    const TextFieldItem(label: 'Student’s NSBM ID No'),
-                    const TextFieldItem(label: 'Student’s Name'),
-                    const TextFieldItem(label: 'Student’s Intake'),
-                    const TextFieldItem(label: 'Degree Program'),
-                    const TextFieldItem(label: 'Fee Type'),
-                    const TextFieldItem(label: 'Order No'),
-                    const TextFieldItem(label: 'Amount'),
-                    const TextFieldItem(label: 'Currency'),
-                    const SizedBox(height: 16.0),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Next'),
-                    ),
-                  ],
-                ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                  const TextFieldItem(label: 'Student’s NSBM ID No'),
+                  const TextFieldItem(label: 'Student’s Name'),
+                  const TextFieldItem(label: 'Student’s Intake'),
+                  const TextFieldItem(label: 'Degree Program'),
+                  const TextFieldItem(label: 'Fee Type'),
+                  const TextFieldItem(label: 'Order No'),
+                  const TextFieldItem(label: 'Amount'),
+                  const TextFieldItem(label: 'Currency'),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Next'),
+                  ),
+                ],
               ),
             ),
           ],
         ),
-        bottomNavigationBar: CustomBottomNavigationBar(
-          currentIndex: 2, // Set the correct index for EventsPage
-          onTap: (index) {
-            // Handle bottom navigation bar tap event
-            if (index == 0) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
-              );
-            }
-          },
-        ),
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: 2, 
+        onTap: (index) {
+        
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          }
+        },
       ),
     );
   }
@@ -106,7 +72,7 @@ class Online_Payment extends StatelessWidget {
 class TextFieldItem extends StatelessWidget {
   final String label;
 
-  const TextFieldItem({super.key, required this.label});
+  const TextFieldItem({Key? key, required this.label}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
