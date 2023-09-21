99f50753cfe7e26cd6f2d4ac9f98ad094bd5b0f6
Akila Tharinda <96075103+Akilap11@users.noreply.github.com>
2023-09-22 00:25:51 +0530

    bannerREMOVED

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoadingPage());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: LoadingPage());
   }
 }
